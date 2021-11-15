dc6004bf3faf231fd72586f35b2d0be395c8c96f
NotPelos <iezzio4@example.com>
2021-11-15 14:16:06 +0100

    2

---
+++ src/main/java/com/configserverclient/configserverclient/controllers/ProfileManager.java
@@ -0,0 +1,15 @@
+package com.configserverclient.configserverclient.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class ProfileManager {
+    @Value("${spring.profiles.active:}")
+    private String activeProfiles;
+
+    public String getActiveProfiles() {
+        for (String profileName : activeProfiles.split(",")) {
+            System.out.println("Currently active profile - " + profileName);
+        }
+		return activeProfiles;
+    }
+}
